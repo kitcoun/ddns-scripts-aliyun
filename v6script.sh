7b85fac47c9d8f88d84565dcf0c0021788b10f48
Emily Johnson <38834202+kitcoun@users.noreply.github.com>
2025-10-20 16:08:31 +0800

    Change command to show IPv6 addresses with suffix

--- v6script.sh
+++ v6script.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
 INTERF=br-lan
 SUFFIX="30:9c:23:68:db:28"
-ip -6 neigh show dev $INTERF | grep "2409.*$SUFFIX" | awk '{print $1}'
+ip -6 addr show dev $INTERF | awk '/inet6/ && !/fe80::|deprecated/ {print $2}' | cut -d':' -f1-4 | sed "s/$/:$SUFFIX/"
