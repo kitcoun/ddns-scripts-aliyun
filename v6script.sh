6c9f6e63ff832f214cbb70131c9a3ee83453eee7
Emily Johnson <38834202+kitcoun@users.noreply.github.com>
2025-10-20 16:21:15 +0800

    Change IP command to use 'neigh' instead of 'addr'

--- v6script.sh
+++ v6script.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
 INTERF=br-lan
 SUFFIX="30:9c:23:68:db:28"
-ip -6 addr show dev $INTERF | awk '/inet6/ && !/fe80::|deprecated/ {print $2}' | cut -d':' -f1-4 | sed "s/$/:$SUFFIX/"
+ip -6 neigh show dev br-lan | grep -i "$SUFFIX" | awk '/^2409:[0-9a-f]+:/ {print $1}' | head -1
